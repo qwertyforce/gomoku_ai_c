cec05871cdb7ba8595ee710c66475436fa49072a
qwertyforce <44163887+qwertyforce@users.noreply.github.com>
2020-08-02 15:35:46 +0300

    mtdf

---
+++ mtdf_depth_limited/mtdf_depth_limited.h
@@ -0,0 +1,8 @@
+﻿// mtdf_depth_limited.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
